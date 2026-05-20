064a9d6ee6a184293fa65ae1923e5db068d24148
2025sl93002 <2025sl93002@wilp.bits-pilani.ac.in>
2026-05-21 03:03:40 +0530

    Added Docker support

--- calculator.py
+++ calculator.py
@@ -1,32 +1,18 @@
 def add(a, b):
-<<<<<<< HEAD
     return a + b
-=======
-    return a + b   
->>>>>>> c1cbbe36711d3dff92b16292174a8670ddd7a828
+
+def subtract(a, b):
+    return a - b
 
 def multiply(a, b):
     return a * b
 
-<<<<<<< HEAD
-def divide(a, b):
-    return a / b
-=======
-def subtract(a, b):
-    return a - b
-
 def divide(a, b):
     if b == 0:
-        return None
+        return "Cannot divide by zero"
     return a / b
 
-
-# ---------------- TEST CASES ----------------
-
-print("Running test cases...")
-
-# this should be 15, but add() is wrong → pipeline will fail
-assert add(10, 5) == 15
-
-print("All tests passed")
->>>>>>> c1cbbe36711d3dff92b16292174a8670ddd7a828
+print("Add:", add(10, 5))
+print("Subtract:", subtract(10, 5))
+print("Multiply:", multiply(10, 5))
+print("Divide:", divide(10, 5))
